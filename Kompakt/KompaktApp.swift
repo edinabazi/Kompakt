89a6fca9799711a2fd7eafd360dc49a9f08fea3c
Edin Abazi <edinchezz@gmail.com>
2026-05-16 21:19:35 +0200

    Initial Commit

---
+++ Kompakt/KompaktApp.swift
@@ -0,0 +1,17 @@
+//
+//  KompaktApp.swift
+//  Kompakt
+//
+//  Created by edin on 16/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct KompaktApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KompaktTests/KompaktTests.swift
@@ -0,0 +1,19 @@
+//
+//  KompaktTests.swift
+//  KompaktTests
+//
+//  Created by edin on 16/05/2026.
+//
+
+import Testing
+@testable import Kompakt
+
+struct KompaktTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
